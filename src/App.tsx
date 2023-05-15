ba8fb4c4fa06a97d622146b41958a331825facbb
Oleksandr <didyshen.oleksandr@gmail.com>
2023-05-15 17:12:24 +0300

    Replace useScrollToTop

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
-import ScrollToTop from 'components/ScrollToTop';
-import Header from 'components/Header';
 import Home from 'pages/Home';
 import Games from 'pages/Games';
 import Game from 'pages/Game';
+import Header from 'components/Header';
+import useScrollToTop from 'hooks/useScrollToTop';
 import 'styles/normalize.css';
 import 'styles/fonts.css';
 
 function App() {
   const location = useLocation();
+  useScrollToTop();
 
   return (
     <>
-      <ScrollToTop />
       <Header />
       <AnimatePresence mode="wait" initial={false}>
         <Routes location={location} key={location.pathname}>
